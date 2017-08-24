a618567221362f374d486a9181837a9c759d1b33
HeberTorres <HebeTorres3107@gmail.com>
2017-08-24 09:35:24 -0600

    Initial Commit adding structure of directories and creating the first gg

---
+++ www/js/circulo.js
@@ -0,0 +1,4 @@
+var r = 9;
+var pi = 3.1416;
+var acirculo = pi * Math.pow(r, 2);
+console.log("Area del circulo es= " + acirculo);
---
+++ www/js/triangulo.js
@@ -0,0 +1,4 @@
+var b = 4;
+var h = 6;
+areat = (b * h) / 2
+console.log("El area del triangulo es: " + areat);
