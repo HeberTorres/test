aefd71bda77f7ae298435fbf26561b5c64e6ebf4
HeberTorres <HebeTorres3107@gmail.com>
2017-08-24 09:25:44 -0600

    Initial Commit adding structure of directories and creating the first file

---
+++ www/js/app.js
@@ -0,0 +1,4 @@
+var x = 5;
+var y = 7;
+var area = x * y;
+console.log("Area de rectangulo= " + area);
---
+++ www/js/esfera.js
@@ -0,0 +1,4 @@
+var r = 9;
+var pi = 3.1416;
+var volumen = 1.333 * pi * Math.pow(r, 3);
+console.log("Volumen de la esfera: " + volumen);
